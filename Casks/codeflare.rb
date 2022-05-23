a7db1dd73caabcaccd37d9653fc2c91fd13a4932
Nick Mitchell <nickm@us.ibm.com>
2022-05-23 10:40:30 -0400

    chore: update cask sha256sums

--- Casks/codeflare.rb
+++ Casks/codeflare.rb
@@ -7,11 +7,11 @@ cask "codeflare" do
 
   if Hardware::CPU.intel?
     url "https://github.com/guidebooks/CodeFlareApp/releases/download/v#{version}/CodeFlare-darwin-x64.tar.bz2"
-    sha256 "78902a5b2f81f9f657bf86c469fff1e218815bdd69d61c31f66fa22f4fc36a7e"
+    sha256 "c82ef5514d2bfb0c1d436207240aab04adeb15024b4e9fbb688925447ecc3deb"
     app "CodeFlare-darwin-x64/CodeFlare.app"
   else
     url "https://github.com/guidebooks/CodeFlareApp/releases/download/v#{version}/CodeFlare-darwin-amd64.tar.bz2"
-    sha256 "ba1b090e82fb9401adabd36ed3c157421fbd67d8a21086bb742c4d5bf33e46f4"
+    sha256 "c9f91f118a981154b10d421bcd59e22f47bc96dddaa4377a0e5b69e16df7be85"
     app "CodeFlare-darwin-amd64/CodeFlare.app"
   end
 
